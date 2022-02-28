10eb58b85fc0fe55aeb0b67c5a60017b82064a1e
CS-Buran <62851064+CS-Buran@users.noreply.github.com>
2022-02-28 13:01:39 -0800

    Add files via upload

---
+++ Assignment 1/Homework1.py
@@ -0,0 +1,66 @@
+#        Class: CS 582: Intro to Speech Processing
+#      Purpose: This homework will help to introduce you to Python and Colab
+# Instructions:
+#     1. Write Python code as indicated
+#     2. Turn in this template with working code for each exercise.
+#     3. Turn in a sample run to demonstrate your work
+#
+#        Title: Homework 1
+#          Due: 2.15.2021
+#         Name:
+# Turn-in Date:
+#    File Name: HW1_<LASTNAME>_<LAST_4_DIGITS_OF_STUDENT_ID>.py
+#
+print ("Homework 1: Intro to Python")
+# Replace <LASTNAME> with your last name
+# Replace <LAST_4_DIGITS_OF_STUDENT_ID> with the last 4 digits of your student ID.
+print ("<LASTNAME>_<LAST_4_DIGITS_OF_STUDENT_ID>")
+print ("********** Exercise 1.1 **********")
+# Create and print examples of the basic Python types
+# Write a function named ex1() containing the following functionality:
+# 1. An integer 1 named 'int1'
+
+# 2. A floating point number = 2.0 named 'float1'
+
+# 3. A complex number containing the value 3 - 3i named 'complex1'
+
+# 4. A string containg the value "Hello World" named 'string1'
+
+# 5. A list containing the values 'a', 0, 'b', '2', 'c', 3 named 'list1'
+
+# 6. A dictionary named 'dictionary1' containing the values 1,2,3,4
+
+# 7. A tuple named 'tuple1' containg the values 0,1
+
+# 8. A boolean named 'boolean1' = false
+# 9. Invoke ex1() to print results
+
+print ("********** Exercise 1.2 **********")
+# Write your code below each prompt
+# Create a function called ex2() which:
+# 1. Prompts the user for their name
+# 2. Writes the text entry to the file tmp1.txt
+# 3. Reads the value from the file
+# 4. Print the value read from the file
+# 5. Invoke ex2() to print results
+
+print ("********** Exercise 1.3 **********")
+# Write a function called ex3(n) which
+# 1. accepts as an input parameter an integer between 1 and 100
+# 2. Calculates and print the mersenne primes between 1 and this number
+#    See en.wikipedia.org/wiki/Mersenne_prime for details
+# 3. Invoke ex3(16) to print results
+
+print ("********** Exercise 1.4 **********")
+# Write a function called ex4() which implements a "Guess the number" game
+# 1. Generate a random number between 1 and 100
+#    See https://docs.python.org/3/library/random.html for random number
+#    generation details
+# 1. Prompt the user with the rules of the game ('q' to quit)
+# 2. Iterively prompt a user for a guess
+# 3. Let the user know whether their last guess was high or low
+# 4. Count the number of guesses
+# 5. Congratulate them upon success. Print the number of guesses
+# 6. Prompt to play again or quit
+# 7. Proceed as directed by user.
+# 8. Invoke ex4() and play a game to print results
